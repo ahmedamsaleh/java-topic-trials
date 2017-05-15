a347a3224061e87e3c096068e5163c49715c27ed
jhtrujil <jhtrujil@us.ibm.com>
2017-05-15 12:06:42 -0400

    create test file

---
+++ unusual-spending/src/main/test/TriggersUnusualSpendingEmailTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TriggersUnusualSpendingEmailTest {
+    @Test
+    public void canaryTest() {
+        assertTrue(true);
+    }
+}
